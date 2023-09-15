d516c16a8655b0516207ebc3a32227ca11d45cdd
Alex Forsythe <awforsythe@gmail.com>
2023-09-15 17:03:17 -0400

    Initial commit: placeholder extension app with hardcoded data

---
+++ README.md
@@ -0,0 +1,10 @@
+# extensions
+
+The Twitch **extensions** repo contains the source of the frontend apps that are
+rendered within the Twitch app to enrich the viewer experience with additional
+information and interactivity:
+
+- [Twitch Extensions](https://dev.twitch.tv/docs/extensions/)
+
+This repo currently contains a single extension: an app that's intended to render
+information about the tape that's currently being viewed.
---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<main>
+  <img src="https://golden-vcr-images.nyc3.digitaloceanspaces.com/0058_a.jpg" alt="tape" />
+  <div class="third">
+    <h1>Pressure Washers (DeVilbiss Air Power Company)</h1>
+  </div>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+    padding: 5vh;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    object-position: left;
+  }
+  .third {
+    position: absolute;
+    left: 10vw;
+    right: 0;
+    bottom: 0;
+    height: 30vh;
+    display: flex;
+    flex-direction: column;
+  }
+  h1 {
+    width: fit-content;
+    padding: 1rem 2rem;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+</style>
